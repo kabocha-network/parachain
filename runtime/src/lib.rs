11e02ce5789367cdf7faf5b480692b0cd52a0cba
Junte <madtodist@gmail.com>
2022-02-08 11:48:43 +0100

    changed back let p= MILLIUNIT, accidently changed it to MICROUNIT before when I was renaming.

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -138,7 +138,7 @@ impl WeightToFeePolynomial for WeightToFee {
 	fn polynomial() -> WeightToFeeCoefficients<Self::Balance> {
 		// in Rococo, extrinsic base weight (smallest non-zero weight) is mapped to 1 MILLIUNIT:
 		// in our template, we map to 1/10 of that, or 1/10 MILLIUNIT
-		let p = MICROUNIT / 10;
+		let p = MILLIUNIT / 10;
 		let q = 100 * Balance::from(ExtrinsicBaseWeight::get());
 		smallvec![WeightToFeeCoefficient {
 			degree: 1,

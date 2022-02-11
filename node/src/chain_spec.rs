73a274e18062fbb4481885c06c79f9f61289c8fa
decentration <ramseyajram@me.com>
2022-02-11 14:28:45 +0000

    chain specs and wasms

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -64,16 +64,16 @@ pub fn template_session_keys(keys: AuraId) -> kabocha_runtime::SessionKeys {
 pub fn development_config() -> ChainSpec {
 	// Give your base currency a unit name and decimal places
 	let mut properties = sc_chain_spec::Properties::new();
-	properties.insert("tokenSymbol".into(), "UNIT".into());
+	properties.insert("tokenSymbol".into(), "KAB".into());
 	properties.insert("tokenDecimals".into(), 12.into());
-	properties.insert("ss58Format".into(), 42.into());
+	properties.insert("ss58Format".into(), 27.into());
 
 	ChainSpec::from_genesis(
 		// Name
 		"Development",
 		// ID
 		"dev",
-		ChainType::Development,
+		ChainType::Live,
 		move || {
 			testnet_genesis(
 				// initial collators.

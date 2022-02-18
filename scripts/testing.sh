715f353b8af0ac0462c71e78ff4b7fb833026ae7
decentration <ramseyajram@me.com>
2022-02-18 10:50:38 +0000

    specs and wasms, and benchmark fix

---
+++ scripts/testing.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#be sure to add chmod +x basic_script.sh when making an excutor script
+
+echo "what is your name?"
+
+read name
+
+echo "How do you do, $name?"
+
+read remark
+
+echo "I am $remark too!"

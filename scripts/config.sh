15c8768933ea4d7f5b834a525ea6cac86639dc00
decentration <ramseyajram@me.com>
2022-03-02 21:35:26 +0000

    clean up build scripts

--- scripts/config.sh
+++ scripts/config.sh
@@ -6,15 +6,19 @@ export CHAIN_TYPE=parachain
 export RELAY_CHAIN_SPEC="../specs/pop-art-3-val.json"
 export CHAIN_NAME="parachain-collator"
 export PROTOCOL_ID=soupcan
-export SPEC_NAME="kabocha-soupcan-dev-plain-v0.9.17.json"
-export PARA_ID=2007
+export SPEC_NAME="kabocha-soupcan-dev-plain-v0.9.17-new.json"
 export RAWSPEC_NAME='kabocha-'"$PARA_ID"'.json'
+
+export PARA_ID=2068
+
+export GENESIS_HEAD='../genesiswasm/'"$PROTOCOL_ID"'-'"$PARA_ID"'-genesis'
+export GENESIS_WASM='../genesiswasm/'"$PROTOCOL_ID"'-'"$PARA_ID"'-wasm'
+export BASE_PATH="/home/$USERNAME/$CHAIN_TYPE"
+
+
 export PORT=30333
 export WS_PORT=9944
 export RPC_PORT=9933
 export RELAY_PORT=40333
 export RELAY_WS_PORT=8844
 export USERNAME=decentration
-export GENESIS_HEAD='../genesiswasm/'"$PROTOCOL_ID"'-'"$PARA_ID"'-genesis'
-export GENESIS_WASM='../genesiswasm/'"$PROTOCOL_ID"'-'"$PARA_ID"'-wasm'
-export BASE_PATH="/home/$USERNAME/$CHAIN_TYPE"

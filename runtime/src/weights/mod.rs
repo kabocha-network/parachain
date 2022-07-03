15972ba056fcd21af78bfe687b711e508f032b78
decentration <ramseyajram@me.com>
2022-07-03 17:09:30 +0000

    spec and lock

--- runtime/src/weights/mod.rs
+++ runtime/src/weights/mod.rs
@@ -47,7 +47,7 @@ pub use rocksdb_weights::constants::RocksDbWeight;
 
 pub mod frame_system;
 pub mod pallet_balances;
-pub mod pallet_vesting;
+// pub mod pallet_vesting;
 pub mod pallet_multisig;
 pub mod pallet_timestamp;
 pub mod pallet_collator_selection;

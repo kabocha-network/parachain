18d5203c64cae665cbdf04f39bfd0c0f05149364
decentration <ramseyajram@me.com>
2022-09-30 11:44:04 +0000

    bump spec version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -182,7 +182,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("kabocha-parachain"),
 	impl_name: create_runtime_str!("kabocha-parachain"),
 	authoring_version: 3,
-	spec_version: 26,
+	spec_version: 27,
 	impl_version: 4,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 2,

63b01c048f5a35931376d2178558cdd2023984e4
decentration <ramseyajram@me.com>
2022-10-13 14:58:51 +0000

    update to specs

--- scripts/config.sh
+++ scripts/config.sh
@@ -3,13 +3,13 @@
 
 #VARIABLES
 export CHAIN_TYPE=parachain
-export RELAY_CHAIN_SPEC="../specs/pop-art-3-val.json"
+export RELAY_CHAIN_SPEC="../specs/pop-art-3-val-oct.json"
 export CHAIN_NAME="parachain-collator"
 export PROTOCOL_ID=kabocha
-export SPEC_NAME="soup-2015-plain.json"
-export RAWSPEC_NAME="soup-2015-raw.json"
+export SPEC_NAME="soup-2000-plain.json"
+export RAWSPEC_NAME="soup-2001-raw.json"
 
-export PARA_ID=2015
+export PARA_ID=2001
 
 export GENESIS_HEAD='../genesiswasm/'"$PROTOCOL_ID"'-'"$PARA_ID"'-genesis'
 export GENESIS_WASM='../genesiswasm/'"$PROTOCOL_ID"'-'"$PARA_ID"'-wasm'

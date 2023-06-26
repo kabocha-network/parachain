7a39cddcc6d788a87cc9b8b6e67d1872d3e6ac4e
decentration <ramseyajram@me.com>
2023-06-26 09:43:05 +0000

    ad soupcan 2012

--- scripts/config.sh
+++ scripts/config.sh
@@ -6,10 +6,10 @@ export CHAIN_TYPE=parachain
 export RELAY_CHAIN_SPEC="../specs/pop-art-2023-raw.json"
 export CHAIN_NAME="parachain-collator"
 export PROTOCOL_ID=kabocha
-export SPEC_NAME="soup-2002-plain-for-uprade.json"
-export RAWSPEC_NAME="soup-2002-raw.json"
+export SPEC_NAME="soup-2012.json"
+export RAWSPEC_NAME="soup-2012-raw.json"
 
-export PARA_ID=2002
+export PARA_ID=2012
 
 export GENESIS_HEAD='../genesiswasm/'"$PROTOCOL_ID"'-'"$PARA_ID"'-genesis'
 export GENESIS_WASM='../genesiswasm/'"$PROTOCOL_ID"'-'"$PARA_ID"'-wasm'
